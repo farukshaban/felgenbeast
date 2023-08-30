b98a01bf1b8904dd8839a27f2a1cc0c1f7cd3f55
Faruk Shaban <faruk.saban@live.com>
2023-08-30 11:36:10 +0200

    app js changed with header from pwxlab

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,27 @@
-import '../styles/global.css'
+import React, { Fragment } from 'react'
+import { Header } from '@pxwlab/katana-snx'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function Demo() {
+  return (
+    <Fragment>
+      <Header onSearch={(data) => console.log(data)}>
+        <Header.NavigationItem label='1st level' href='#' />
+        <Header.NavigationItem label='1st level nested'>
+          <Header.NavigationItem label='2nd level' href='#' />
+          <Header.NavigationItem label='2nd level' href='#' />
+          <Header.NavigationItem label='2nd level' href='#' />
+        </Header.NavigationItem>
+        <Header.NavigationItem label='1st level 2xnested'>
+          {/* Note the nested prop here */}
+          <Header.NavigationItem label='2nd level nested' nested>
+            <Header.NavigationItem label='3rd level' href='#' />
+            <Header.NavigationItem label='3rd level' href='#' />
+            <Header.NavigationItem label='3rd level' href='#' />
+          </Header.NavigationItem>
+        </Header.NavigationItem>
+      </Header>
+    </Fragment>
+  )
 }
+
+export default Demo
